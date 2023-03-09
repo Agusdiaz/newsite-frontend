7a835921d417147e64c20191cdbe280a1bdbf72d
Agus Days <agus--diaz@hotmail.com>
2023-03-09 20:07:28 -0300

    icons + global styles

---
+++ src/assets/iconsSVG.js
@@ -0,0 +1,323 @@
+import "./icons.scss";
+
+const LightTheme = () => {
+  return (
+    <svg
+      className="svgContainer"
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      width="7rem"
+      height="7rem"
+      viewBox="0 0 1080 1080"
+    >
+      <rect x="0" y="0" width="100%" height="100%" fill="transparent"></rect>
+      <g
+        transform="matrix(1 0 0 1 540 540)"
+        id="5835655e-55b0-4136-a8ab-e4556f2b42d4"
+      >
+        <rect
+          style={{
+            stroke: "none",
+            strokeWidth: 1,
+            strokeDasharray: "none",
+            strokeLinecap: "butt",
+            strokeDashoffset: 0,
+            strokeLinejoin: "miter",
+            strokeMiterlimit: 4,
+            fillRule: "nonzero",
+            opacity: 1,
+            visibility: "hidden",
+          }}
+          vector-effect="non-scaling-stroke"
+          x="-540"
+          y="-540"
+          rx="0"
+          ry="0"
+          width="1080"
+          height="1080"
+        />
+      </g>
+      <g
+        transform="matrix(1 0 0 1 540 540)"
+        id="fb200e0e-d0ae-4cf9-b00d-4a8ea5d3083c"
+      ></g>
+      <g
+        transform="matrix(13.22 0 0 13.22 542.7 542.7)"
+        id="4051e2d6-5c05-4432-a110-f0f7159c151d"
+      >
+        <path
+          style={{
+            strokeWidth: 5,
+            strokeDasharray: "none",
+            strokeLinecap: "butt",
+            strokeDashoffset: 0,
+            strokeLinejoin: "miter",
+            strokeMiterlimit: 4,
+            fillRule: "nonzero",
+            opacity: 1,
+          }}
+          vector-effect="non-scaling-stroke"
+          transform=" translate(-50, -50)"
+          d="M 90.375 90.375 L 9.625 90.375 L 9.625 9.625 L 90.375 9.625 L 90.375 90.375 z M 11.625 88.375 L 88.375 88.375 L 88.375 11.625 L 11.625 11.625 L 11.625 88.375 z"
+          stroke-linecap="round"
+        />
+      </g>
+      <g transform="matrix(1 0 0 1 540 540)">
+        <g vector-effect="non-scaling-stroke">
+          <g transform="matrix(1.97 0 0 1.97 0 0.01)">
+            <path
+              style={{
+                stroke: "none",
+                strokeWidth: 1,
+                strokeDasharray: "none",
+                strokeLinecap: "butt",
+                strokeDashoffset: 0,
+                strokeLinejoin: "miter",
+                strokeMiterlimit: 4,
+                fillRule: "nonzero",
+                opacity: 1,
+              }}
+              vector-effect="non-scaling-stroke"
+              transform=" translate(-202.95, -202.95)"
+              d="M 202.948 118.395 C 156.32500000000002 118.395 118.387 156.326 118.387 202.949 C 118.387 249.58 156.325 287.511 202.948 287.511 C 249.572 287.511 287.509 249.58000000000004 287.509 202.949 C 287.509 156.325 249.571 118.395 202.948 118.395 z M 202.948 259.451 C 171.793 259.451 146.446 234.10200000000003 146.446 202.949 C 146.446 171.80100000000002 171.793 146.454 202.948 146.454 C 234.103 146.454 259.45 171.80100000000002 259.45 202.949 C 259.45 234.102 234.103 259.451 202.948 259.451 z"
+              stroke-linecap="round"
+            />
+          </g>
+          <g transform="matrix(1.97 0 0 1.97 0 -302.84)">
+            <path
+              style={{
+                stroke: "none",
+                strokeWidth: 1,
+                strokeDasharray: "none",
+                strokeLinecap: "butt",
+                strokeDashoffset: 0,
+                strokeLinejoin: "miter",
+                strokeMiterlimit: 4,
+                fillRule: "nonzero",
+                opacity: 1,
+              }}
+              vector-effect="non-scaling-stroke"
+              transform=" translate(-202.95, -49.3)"
+              d="M 202.948 98.59 C 210.697 98.59 216.978 92.308 216.978 84.561 L 216.978 14.03 C 216.978 6.281999999999999 210.697 0.0009999999999994458 202.948 0.0009999999999994458 C 195.20000000000002 0.0009999999999994458 188.919 6.281999999999999 188.919 14.03 L 188.919 84.561 C 188.918 92.308 195.199 98.59 202.948 98.59 z"
+              stroke-linecap="round"
+            />
+          </g>
+          <g transform="matrix(1.97 0 0 1.97 214.14 -214.13)">
+            <path
+              style={{
+                stroke: "none",
+                strokeWidth: 1,
+                strokeDasharray: "none",
+                strokeLinecap: "butt",
+                strokeDashoffset: 0,
+                strokeLinejoin: "miter",
+                strokeMiterlimit: 4,
+                fillRule: "nonzero",
+                opacity: 1,
+              }}
+              vector-effect="non-scaling-stroke"
+              transform=" translate(-311.59, -94.31)"
+              d="M 286.659 133.274 C 290.24899999999997 133.274 293.846 131.904 296.585 129.164 L 346.45599999999996 79.28599999999999 C 351.92999999999995 73.80699999999999 351.92999999999995 64.92099999999999 346.448 59.447999999999986 C 340.969 53.96799999999999 332.091 53.96799999999999 326.60999999999996 59.447999999999986 L 276.739 109.32499999999999 C 271.25899999999996 114.80499999999999 271.25899999999996 123.68999999999998 276.739 129.164 C 279.479 131.904 283.069 133.274 286.659 133.274 z"
+              stroke-linecap="round"
+            />
+          </g>
+          <g transform="matrix(1.97 0 0 1.97 302.84 0.01)">
+            <path
+              style={{
+                stroke: "none",
+                strokeWidth: 1,
+                strokeDasharray: "none",
+                strokeLinecap: "butt",
+                strokeDashoffset: 0,
+                strokeLinejoin: "miter",
+                strokeMiterlimit: 4,
+                fillRule: "nonzero",
+                opacity: 1,
+              }}
+              vector-effect="non-scaling-stroke"
+              transform=" translate(-356.6, -202.95)"
+              d="M 391.866 188.919 L 321.335 188.92600000000002 C 313.58099999999996 188.92600000000002 307.30699999999996 195.20700000000002 307.30699999999996 202.955 C 307.30699999999996 210.703 313.58899999999994 216.984 321.335 216.984 L 391.866 216.977 C 399.614 216.977 405.897 210.697 405.897 202.948 C 405.897 195.2 399.614 188.919 391.866 188.919 z"
+              stroke-linecap="round"
+            />
+          </g>
+          <g transform="matrix(1.97 0 0 1.97 214.13 214.15)">
+            <path
+              style={{
+                stroke: "none",
+                strokeWidth: 1,
+                strokeDasharray: "none",
+                strokeLinecap: "butt",
+                strokeDashoffset: 0,
+                strokeLinejoin: "miter",
+                strokeMiterlimit: 4,
+                fillRule: "nonzero",
+                opacity: 1,
+              }}
+              vector-effect="non-scaling-stroke"
+              transform=" translate(-311.59, -311.6)"
+              d="M 296.571 276.748 C 291.091 271.269 282.21400000000006 271.269 276.733 276.748 C 271.252 282.227 271.253 291.113 276.733 296.587 L 326.61 346.457 C 329.35 349.197 332.94 350.566 336.53000000000003 350.566 C 340.12000000000006 350.566 343.716 349.19699999999995 346.456 346.457 C 351.93 340.978 351.93 332.092 346.44800000000004 326.61899999999997 L 296.571 276.748 z"
+              stroke-linecap="round"
+            />
+          </g>
+          <g transform="matrix(1.97 0 0 1.97 -0.01 302.84)">
+            <path
+              style={{
+                stroke: "none",
+                strokeWidth: 1,
+                strokeDasharray: "none",
+                strokeLinecap: "butt",
+                strokeDashoffset: 0,
+                strokeLinejoin: "miter",
+                strokeMiterlimit: 4,
+                fillRule: "nonzero",
+                opacity: 1,
+              }}
+              vector-effect="non-scaling-stroke"
+              transform=" translate(-202.94, -356.6)"
+              d="M 202.934 307.306 C 195.186 307.306 188.912 313.58799999999997 188.912 321.334 L 188.919 391.865 C 188.919 399.613 195.20000000000002 405.896 202.955 405.896 C 210.703 405.896 216.978 399.613 216.978 391.865 L 216.97 321.334 C 216.97 313.589 210.688 307.306 202.934 307.306 z"
+              stroke-linecap="round"
+            />
+          </g>
+          <g transform="matrix(1.97 0 0 1.97 -214.15 214.14)">
+            <path
+              style={{
+                stroke: "none",
+                strokeWidth: 1,
+                strokeDasharray: "none",
+                strokeLinecap: "butt",
+                strokeDashoffset: 0,
+                strokeLinejoin: "miter",
+                strokeMiterlimit: 4,
+                fillRule: "nonzero",
+                opacity: 1,
+              }}
+              vector-effect="non-scaling-stroke"
+              transform=" translate(-94.3, -311.6)"
+              d="M 109.303 276.74 L 59.44 326.619 C 53.965999999999994 332.098 53.965999999999994 340.98400000000004 59.446999999999996 346.45700000000005 C 62.187 349.19700000000006 65.776 350.56600000000003 69.366 350.56600000000003 C 72.956 350.56600000000003 76.54599999999999 349.197 79.285 346.45700000000005 L 129.149 296.58000000000004 C 134.629 291.10100000000006 134.629 282.21600000000007 129.149 276.74000000000007 C 123.668 271.26 114.783 271.26 109.303 276.74 z"
+              stroke-linecap="round"
+            />
+          </g>
+          <g transform="matrix(1.97 0 0 1.97 -302.84 0)">
+            <path
+              style={{
+                stroke: "none",
+                strokeWidth: 1,
+                strokeDasharray: "none",
+                strokeLinecap: "butt",
+                strokeDashoffset: 0,
+                strokeLinejoin: "miter",
+                strokeMiterlimit: 4,
+                fillRule: "nonzero",
+                opacity: 1,
+              }}
+              vector-effect="non-scaling-stroke"
+              transform=" translate(-49.29, -202.95)"
+              d="M 98.589 202.949 C 98.589 195.20100000000002 92.307 188.92000000000002 84.56 188.92000000000002 L 14.029 188.92000000000002 C 6.281 188.92 0 195.201 0 202.949 C 0 210.698 6.281 216.979 14.029 216.979 L 84.56 216.979 C 92.307 216.977 98.589 210.697 98.589 202.949 z"
+              stroke-linecap="round"
+            />
+          </g>
+          <g transform="matrix(1.97 0 0 1.97 -214.14 -214.14)">
+            <path
+              style={{
+                stroke: "none",
+                strokeWidth: 1,
+                strokeDasharray: "none",
+                strokeLinecap: "butt",
+                strokeDashoffset: 0,
+                strokeLinejoin: "miter",
+                strokeMiterlimit: 4,
+                fillRule: "nonzero",
+                opacity: 1,
+              }}
+              vector-effect="non-scaling-stroke"
+              transform=" translate(-94.3, -94.3)"
+              d="M 109.317 129.15 C 112.05699999999999 131.89000000000001 115.64699999999999 133.26000000000002 119.237 133.26000000000002 C 122.826 133.26000000000002 126.416 131.89000000000001 129.156 129.15 C 134.636 123.67 134.636 114.78500000000001 129.156 109.311 L 79.285 59.448 C 73.806 53.968 64.92 53.968 59.44 59.448 C 53.965999999999994 64.927 53.965999999999994 73.813 59.446999999999996 79.286 L 109.317 129.15 z"
+              stroke-linecap="round"
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+const DarkTheme = () => {
+  return (
+    <svg
+      className="svgContainer"
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      width="7rem"
+      height="7rem"
+      viewBox="0 0 1080 1080"
+    >
+      <g
+        transform="matrix(1 0 0 1 540 540)"
+        id="5835655e-55b0-4136-a8ab-e4556f2b42d4"
+      >
+        <rect
+          style={{
+            stroke: "none",
+            strokeWidth: 1,
+            strokeDasharray: "none",
+            strokeLinecap: "butt",
+            strokeDashoffset: 0,
+            strokeLinejoin: "miter",
+            strokeMiterlimit: 4,
+            fillRule: "nonzero",
+            opacity: 1,
+            visibility: "hidden",
+          }}
+          vector-effect="non-scaling-stroke"
+          x="-540"
+          y="-540"
+          rx="0"
+          ry="0"
+          width="1080"
+          height="1080"
+        />
+      </g>
+      <g
+        transform="matrix(13.22 0 0 13.22 542.7 542.7)"
+        id="4051e2d6-5c05-4432-a110-f0f7159c151d"
+      >
+        <path
+          style={{
+            strokeWidth: 5,
+            strokeDasharray: "none",
+            strokeLinecap: "butt",
+            strokeDashoffset: 0,
+            strokeLinejoin: "miter",
+            strokeMiterlimit: 4,
+            fillRule: "nonzero",
+            opacity: 1,
+          }}
+          vector-effect="non-scaling-stroke"
+          transform=" translate(-50, -50)"
+          d="M 90.375 90.375 L 9.625 90.375 L 9.625 9.625 L 90.375 9.625 L 90.375 90.375 z M 11.625 88.375 L 88.375 88.375 L 88.375 11.625 L 11.625 11.625 L 11.625 88.375 z"
+          stroke-linecap="round"
+        />
+      </g>
+      <g transform="matrix(24.85 0 0 24.85 543.49 540)" id="Path_70">
+        <path
+          style={{
+            strokeWidth: 0,
+            strokeDasharray: "none",
+            strokeLinecap: "butt",
+            strokeDashoffset: 0,
+            strokeLinejoin: "miter",
+            strokeMiterlimit: 4,
+            fillRule: "nonzero",
+            opacity: 1,
+          }}
+          vector-effect="non-scaling-stroke"
+          transform=" translate(-15.99, -16)"
+          d="M 12.516 4.509 C 11.496491094069537 7.860293142989306 11.992230750490073 11.489752716407162 13.873120285594215 14.444887010522134 C 15.754009820698357 17.400021304637107 18.832247246415974 19.385767751929357 22.299999999999997 19.881 C 22.86323965846137 19.959867643937447 23.431265544545575 19.99962945596334 24 20 C 25.182319177307193 20.001763377766622 26.358391164235357 19.828553921812155 27.490000000000002 19.485999999999997 C 26.096003801006578 23.99614231687759 22.20633987604715 27.283792614185966 17.527 27.907 C 17.02033589827165 27.968707327338393 16.510407925543824 27.999763844341462 16 28 C 10.042592468766308 28.013015654585327 4.977621099583134 23.65347918297564 4.103623404995339 17.76051711536317 C 3.2296257104075448 11.867555047750706 6.811258004931297 6.22562245999633 12.515999999999991 4.509000000000004 M 16 0 C 15.260137567871777 0.00035024218742194093 14.521136409264312 0.05046334064633669 13.787999999999984 0.15000000000000213 C 5.450378298435204 1.299691355589727 -0.5710877162489698 8.712270170632257 0.01203925012246998 17.108560350980504 C 0.5951662164939098 25.504850531328753 7.583496398862341 32.01389185224632 15.999999999999998 32 C 16.67187739659936 31.999897177768645 17.34312326422295 31.958820938108097 18.009999999999998 31.877 C 25.173736155303708 30.957439506328484 30.839359393513476 25.363455290278157 31.85 18.211999999999996 C 31.952023283914052 17.46034164038453 32.002141937785055 16.70254759385483 31.999999999999996 15.943999999999999 C 31.996643867228016 15.419692637454942 31.783037355492258 14.918659613186742 31.40706218318018 14.553211745699404 C 31.031087010868106 14.187763878212065 30.524191285517947 13.988468728032752 30 14 C 29.556051102900003 14.000575587960341 29.124334503842412 14.145534088294314 28.77 14.413 C 27.39378133814733 15.444533509933144 25.719894504036215 16.00144302895502 24 16 C 23.61528143665984 15.999857773139132 23.23103263673293 15.973127421839868 22.85 15.92 C 20.05780377483969 15.517844996662976 17.684209133762938 13.673960437030539 16.604045112368375 11.067941616416764 C 15.523881090973816 8.46192279580299 15.897044666598894 5.479539397373822 17.586 3.220000000000004 C 17.999656776997128 2.613832963656206 18.060343718725566 1.8336094721246818 17.745392137085812 1.1707701957325192 C 17.430440555446058 0.5079309193403567 16.787221610934285 0.06217202592918092 16.056 0 Z"
+          stroke-linecap="round"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export { LightTheme, DarkTheme };
